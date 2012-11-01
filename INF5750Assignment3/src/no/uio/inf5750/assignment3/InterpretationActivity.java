501ae4f04c49ee422431caa734a6bcb800e6b537
Simernes <simen_russnes@hotmail.com>
2012-11-01 11:57:02 +0100

    layout changes and moving from packages

---
+++ INF5750Assignment3/src/no/uio/inf5750/assignment3/InterpretationActivity.java
@@ -0,0 +1,12 @@
+package no.uio.inf5750.assignment3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InterpretationActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.interpretation);
+	}
+}
