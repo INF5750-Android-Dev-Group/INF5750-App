501be71ce5e1682b5670fb9e7a71377ee7023581
Simernes <simen_russnes@hotmail.com>
2012-11-01 20:48:58 +0100

    added listview to messages and fixed a flow thing

--- INF5750Assignment3/src/no/uio/inf5750/assignment3/messaging/MessagingActivity.java
+++ INF5750Assignment3/src/no/uio/inf5750/assignment3/messaging/MessagingActivity.java
@@ -13,33 +13,71 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MessagingActivity extends Activity {
 	/** Called when the activity is first created. */
+	
+	private ListView mListView;
+	private Context mContext;
 	String mMessages = "";
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.messaging);
+
+		mContext = this;
+		
+		setupViews();
+		
 		update();
 	}
+	
+	public void setupViews()
+	{
+		mListView = (ListView) findViewById(R.id.messaging_listview);
+		mListView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                int position, long id) {
+              // When clicked, show a toast with the TextView text
+              Toast.makeText(getApplicationContext(), ((TextView) view).getText().toString(),
+                  Toast.LENGTH_SHORT).show();
+            }
+          });
+	}
+	
 	public void update() {
 		UpdateDaemon.getUpdateDaemon().update();
 		NodeList messages = UpdateDaemon.getUpdateDaemon().getMessages();
 		LinearLayout msgList = (LinearLayout) findViewById(R.id.message_list);
 		if (messages != null) {
+			
+			String[] values = new String[messages.getLength()];
+			
 			for (int i = 0; i < messages.getLength(); i++) {
-				TextView text = new TextView(this);
+				/*TextView text = new TextView(this);
 				NamedNodeMap map = messages.item(i).getAttributes();
 				text.setText(map.getNamedItem("name").getNodeValue());
-				msgList.addView(text);
+				msgList.addView(text);*/
+				values[i] = messages.item(i).getAttributes().getNamedItem("name").getNodeValue();
 			}
+			
+			setListView(values);
 		} 
+	}
+	
+	public void setListView(String[] values)
+	{
+		ArrayAdapter arrayAdapter = new ArrayAdapter(mContext, R.layout.list_item, values);
+    	mListView.setAdapter(arrayAdapter);
 	}
 }
