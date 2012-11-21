086d3328f46ce75ea25a0daeb1b51dce4c1878e3
Subway <sabba_88@hotmail.com>
2012-11-21 17:37:50 +0100

    Can now show the entire message, with reply-textfield.

--- INF5750Assignment3/src/no/uio/inf5750/assignment3/UpdateDaemon.java
+++ INF5750Assignment3/src/no/uio/inf5750/assignment3/UpdateDaemon.java
@@ -1,6 +1,9 @@
 package no.uio.inf5750.assignment3;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
@@ -11,27 +14,28 @@ public class UpdateDaemon {
 	private NodeList mMessageList, mInterpretationList;
 	private HashMap<String, NodeList> mMessages, mInterpretations;
 	private int mUnread;
-	
+
 	static {
 		mDaemon = new UpdateDaemon();
 	}
-	
+
 	public static UpdateDaemon getDaemon() {
 		return mDaemon;
 	}
-	
+
 	private UpdateDaemon() {
 		mMessages = new HashMap<String, NodeList>();
 		mInterpretations = new HashMap<String, NodeList>();
 	}
-	
+
 	public void update() {
 		updateMessages();
 		updateInterpretations();
 	}
-	
+
 	public void updateInterpretations() {
-		String messageList = ConnectionManager.getConnectionManager().doRequest("http://apps.dhis2.org/dev/api/interpretations.xml");
+		String messageList = ConnectionManager.getConnectionManager()
+				.doRequest("http://apps.dhis2.org/dev/api/interpretations.xml");
 		Document doc = Util.getDomElement(messageList);
 		NodeList list = doc.getChildNodes();
 		NodeList metadata = list.item(0).getChildNodes();
@@ -42,12 +46,15 @@ public class UpdateDaemon {
 			}
 		}
 	}
-	
+
 	public void updateMessages() {
-		
-		String messageList = ConnectionManager.getConnectionManager().doRequest("http://apps.dhis2.org/dev/api/messageConversations.xml");
+
+		String messageList = ConnectionManager
+				.getConnectionManager()
+				.doRequest(
+						"http://apps.dhis2.org/dev/api/messageConversations.xml");
 		Document doc = Util.getDomElement(messageList);
-		if(doc==null)
+		if (doc == null)
 			return;
 		NodeList list = doc.getChildNodes();
 		NodeList metadata = list.item(0).getChildNodes();
@@ -58,7 +65,7 @@ public class UpdateDaemon {
 				mMessageList = metadata.item(i).getChildNodes();
 				for (int j = 0; j < mMessageList.getLength(); j++) {
 					NamedNodeMap map = mMessageList.item(j).getAttributes();
-					if ( map.getNamedItem("read").getNodeValue().equals("false") ) {
+					if (map.getNamedItem("read").getNodeValue().equals("false")) {
 						mUnread += 1;
 					}
 				}
@@ -66,44 +73,46 @@ public class UpdateDaemon {
 			}
 		}
 	}
-	
+
 	public NodeList getMessageList() {
 		if (mMessageList == null) {
 			updateMessages();
 		}
 		return mMessageList;
 	}
-	
+
 	public NodeList getInterpretationList() {
 		if (mInterpretationList == null) {
 			updateInterpretations();
 		}
 		return mInterpretationList;
 	}
-	
+
 	public int getUnreadMessages() {
 		if (mMessageList == null) {
 			updateMessages();
 		}
 		return mUnread;
 	}
-	
+
 	public NodeList getMessageContent(String id) {
 		NodeList ret = mMessages.get(id);
 		if (ret == null) {
-			
+
 		}
 		return ret;
 	}
-	
+
 	public String getMessageName(int id) {
-		return getMessageList().item(id).getAttributes().getNamedItem("name").getNodeValue();
+		return getMessageList().item(id).getAttributes().getNamedItem("name")
+				.getNodeValue();
 	}
-	
+
 	public String getMessageUrl(int id) {
-		return getMessageList().item(id).getAttributes().getNamedItem("href").getNodeValue();
+		return getMessageList().item(id).getAttributes().getNamedItem("href")
+				.getNodeValue();
 	}
-	
+
 	public int getNumberOfMessages() {
 		if (mMessageList == null) {
 			updateMessages();
@@ -114,5 +123,41 @@ public class UpdateDaemon {
 			return getMessageList().getLength();
 		}
 	}
-	
+
+	public LinkedHashMap<String, String> getMessage(String url) {
+		LinkedHashMap<String, String> info = new LinkedHashMap<String, String>();
+		String messageList = ConnectionManager.getConnectionManager()
+				.doRequest(url);
+		Document doc = Util.getDomElement(messageList);
+		if (doc == null)
+			return null;
+		NodeList list = doc.getChildNodes();
+		NamedNodeMap mapMessageConversation = list.item(0).getAttributes();
+		info.put("title", mapMessageConversation.getNamedItem("name")
+				.getNodeValue());
+
+		NodeList messages = list.item(0).getChildNodes();
+		for (int i = 0; i < messages.getLength(); i++) {
+			if (messages.item(i).getNodeName().equals("messages")) {
+				NodeList message = messages.item(i).getChildNodes();
+				for (int j = 0; j < message.getLength(); j++) {
+					if(message.item(j).getNodeName().equals("message")){
+						NamedNodeMap textAndDate = message.item(j).getAttributes();
+						info.put(j+"text", textAndDate.getNamedItem("name").getNodeValue());
+						info.put(j+"date", textAndDate.getNamedItem("lastUpdated").getNodeValue().substring(0, 10));
+						NodeList sender = message.item(j).getChildNodes();
+						for(int k = 0; k < sender.getLength(); k++){
+							if (sender.item(k).getNodeName().equals("sender")) {
+								info.put(j+"sender", sender.item(k).getAttributes()
+										.getNamedItem("name").getNodeValue());
+							}
+						}
+					}
+					
+				}
+
+			}
+		}
+		return info;
+	}
 }
--- INF5750Assignment3/src/no/uio/inf5750/assignment3/messaging/MessageActivity.java
+++ INF5750Assignment3/src/no/uio/inf5750/assignment3/messaging/MessageActivity.java
@@ -1,19 +1,34 @@
 package no.uio.inf5750.assignment3.messaging;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import no.uio.inf5750.assignment3.R;
+import no.uio.inf5750.assignment3.UpdateDaemon;
 import no.uio.inf5750.assignment3.interpretation.InterpretationActivity;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MessageActivity extends Activity{
+public class MessageActivity extends Activity {
 	/** Called when the activity is first created. */
 	String mMessage = "";
 	private Button buttonUnread;
@@ -22,7 +37,8 @@ public class MessageActivity extends Activity{
 	private Button buttonUnread2;
 	private Button buttonRemove2;
 	private Button buttonBack2;
-	
+	private Button buttonReply;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -35,99 +51,140 @@ public class MessageActivity extends Activity{
 		} else {
 			mMessage = savedInstanceState.getString("message");
 		}
-		
-		//Add the buttons and their actions
+
+		// Add the buttons and their actions
 		setButtons();
-		
+
 		showMessage();
 	}
-	
-	public void setButtons()
-	{
-		//Create button by id
+
+	public void setButtons() {
+		// Create button by id
 		buttonUnread = (Button) findViewById(R.id.message_unread_button);
 		buttonRemove = (Button) findViewById(R.id.message_remove_button);
 		buttonBack = (Button) findViewById(R.id.message_back_button);
 		buttonUnread2 = (Button) findViewById(R.id.message_unread2_button);
 		buttonRemove2 = (Button) findViewById(R.id.message_remove2_button);
 		buttonBack2 = (Button) findViewById(R.id.message_back2_button);
-		
-		//Set text
+		buttonReply = (Button) findViewById(R.id.message_reply_button);
+
+		// Set text
 		buttonUnread.setText(getString(R.string.message_unread_button));
 		buttonRemove.setText(getString(R.string.message_remove_button));
 		buttonBack.setText(getString(R.string.message_back_button));
 		buttonUnread2.setText(getString(R.string.message_unread_button));
 		buttonRemove2.setText(getString(R.string.message_remove_button));
 		buttonBack2.setText(getString(R.string.message_back_button));
-		
-		//Set onclicklistener
+		buttonReply.setText(getString(R.string.message_reply_button));
+
+		// Set onclicklistener
 		buttonUnread.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				unread();
 			}
 		});
-		
+
 		buttonRemove.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				remove();
 			}
 		});
-		
+
 		buttonBack.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				back();
 			}
 		});
-		
+
 		buttonUnread2.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				unread();
 			}
 		});
-		
+
 		buttonRemove2.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				remove();
 			}
 		});
-		
+
 		buttonBack2.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				back();
 			}
 		});
+
+		buttonReply.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				Toast toast = Toast.makeText(MessageActivity.this,
+						"Reply message", Toast.LENGTH_LONG);
+				toast.show();
+			}
+		});
 	}
-	
-	public void unread(){
-		Toast toast = Toast.makeText(MessageActivity.this,"Mark message as unread", Toast.LENGTH_LONG);  
-        toast.show();
+
+	public void unread() {
+		Toast toast = Toast.makeText(MessageActivity.this,
+				"Mark message as unread", Toast.LENGTH_LONG);
+		toast.show();
 	}
-	
-	public void remove(){
-		Toast toast = Toast.makeText(MessageActivity.this,"Remove message", Toast.LENGTH_LONG);  
-        toast.show();
+
+	public void remove() {
+		Toast toast = Toast.makeText(MessageActivity.this, "Remove message",
+				Toast.LENGTH_LONG);
+		toast.show();
 	}
-	
-	public void back(){
-		Intent intent = new Intent(MessageActivity.this, MessagingActivity.class);
+
+	public void back() {
+		Intent intent = new Intent(MessageActivity.this,
+				MessagingActivity.class);
 		startActivity(intent);
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	public void showMessage() {
 		TextView title = (TextView) findViewById(R.id.message_title);
-		title.setText(mMessage);
+		LinkedHashMap<String, String> info = UpdateDaemon.getDaemon()
+				.getMessage((String) (mMessage + ".xml"));
+		title.setText(info.get("title"));
+		info.remove("title");
+		String[] values = new String[info.size() / 3];
+		// Get a set of the entries and iterator
+		Set set = info.entrySet();
+		Iterator i = set.iterator();
+		Map.Entry<String, String> me;
+		int counter = 0;
+		while (i.hasNext()) {
+			me = (Map.Entry<String, String>) i.next();
+			String text = me.getValue().toString();
+			me = (Map.Entry<String, String>) i.next();
+			String date = me.getValue().toString();
+			me = (Map.Entry<String, String>) i.next();
+			String sender = me.getValue().toString();
+
+			values[counter++] = sender + "\t" + date + "\n\n" + text;
+			Log.d("KEY", me.getKey().toString());
+			Log.d("Value", me.getValue().toString());
+		}
+
+		ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.list_item,
+				values);
+		ListView mListView = (ListView) findViewById(R.id.message_listview);
+		mListView.setAdapter(arrayAdapter);
 	}
 
 }
