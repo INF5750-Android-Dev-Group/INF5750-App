6114f3b791c6a36d2e95a069c6e2bd4a1dd216fa
Gorgeth <wilhelm_huse@hotmail.com>
2012-11-26 12:26:05 +0100

    Interpretations: severe bugs remaining, but runnable

--- INF5750Assignment3/src/no/uio/inf5750/assignment3/interpretation/InterpretationActivity.java
+++ INF5750Assignment3/src/no/uio/inf5750/assignment3/interpretation/InterpretationActivity.java
@@ -15,6 +15,7 @@ import no.uio.inf5750.assignment3.util.Util;
 import no.uio.inf5750.assignment3.util.Interpretation;
 import android.app.Activity;
 import android.content.pm.ApplicationInfo;
+import android.graphics.drawable.Drawable;
 
 import android.os.Bundle;
 
@@ -28,11 +29,15 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.ViewSwitcher;
 
 public class InterpretationActivity extends Activity {
 	private Spinner mSpnInterpretationList;
-	private LinearLayout mLayoutInterpretations;
+	private ViewSwitcher mResourceSwitcher;
+	private LinearLayout mCommentsContainer, mResourceContainer;
 	private ImageView mImageContainer;
+	private TextView mInitialComment, mUserDateInfo, mResourceInformer;
 	private EditText mEditTextInterpretation;
 	private Button mButtonAddInterpretation, mButtonRefresh;
 	
@@ -44,7 +49,7 @@ public class InterpretationActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.interpretations_flipper);
+		setContentView(R.layout.interpretations);
 		
 		//Initializes variables
 		mCurrentInterpretation = 0;
@@ -52,28 +57,6 @@ public class InterpretationActivity extends Activity {
 		
 		//Updates interpretation list
 		update();
-		
-		
-		
-		//Fills out the layout with information from first interpretation
-		//setData(mCurrentInterpretation)
-		
-		//Get user information concerning the interpretations the user can access
-			//Need an integer value that tells the flipper how many layouts it can generate
-				//To be updated when user presses any of the refresh buttons
-		
-		//Create spinner to navigate between the different interpretations
-			//avoids need to load everything to access the last 
-		
-		//Load the text for all interpretations, when user slides just exchange the content of the flipper layouts. 
-			//Charts loads when user flips, use the loading animation/image
-			//Only two layouts inside the flipper.
-		
-		//Load first interpretation
-		//Object chart = null;
-		//loadInterpretation(chart);
-		
-		//On change, remember to clear textedit
 	}
 	
 	//Activity Methods\\
@@ -81,10 +64,8 @@ public class InterpretationActivity extends Activity {
 		//Contacts server to get current interpretations
 		UpdateDaemon.getDaemon().update();
 		
-		
 		//If there are no interpretations there is no need to do anything.
 		if(UpdateDaemon.getDaemon().getNumberOfInterpretations() < 1) return;
-		
 		
 		//Gets list off interpretations from UpdateDaemon
 		mInterpretationList = UpdateDaemon.getDaemon().getInterpretations();
@@ -102,103 +83,99 @@ public class InterpretationActivity extends Activity {
 		ArrayAdapter<String> mSpnAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, mInterpretationNameList);
 		mSpnAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		mSpnInterpretationList.setAdapter(mSpnAdapter);
-			
-		//Creates a TextView that shows interpretation
-		//showInitialInterpretation(mCurrentInterpretation);
 		
-		//Creates a TextView for each comment and adds it into the layout
-		showComments(mCurrentInterpretation);
-		
-		//Load image/chart into the placeholder
-		//showChart(mCurrentInterpretation);
-		
-		//text = mInterpretationList[0].mId;
-		
-		//text = ConnectionManager.getConnectionManager().getSite() + "charts/" + text + "/name";
-		
-		//TextView test = (TextView) findViewById(R.id.interpretationsTextView1);
-		//test.setText(text);
-		
-		
-		/*
-		NamedNodeMap map = interpretations.item(0).getAttributes();
-		/*
-		String URLToInterpretation = ConnectionManager.getConnectionManager().doRequest(map.getNamedItem("href").getNodeValue() + ".xml");
-		
-		NodeList interpretationXML = Util.getDomElement(URLToInterpretation).getChildNodes().item(0).getChildNodes();
-		
-		TextView temp = new TextView(null);
-		
-		//Get thread starter
-		for(int i = 0; i < interpretationXML.getLength(); i++)
-		{
-			if(interpretationXML.item(i).getNodeName().equals("text"))
-			{
-				temp.setText(interpretationXML.item(i).getChildNodes().item(0).getNodeValue());				
-			}
-
-		}
-		mLayoutInterpretations.addView(temp);
-		*/
-		
-		/* TODO: needs a quick fix
-		//Get comments after thread starter
-		for(int i = 0; i < interpretationXML.getLength(); i++)
-		{
-			if(interpretationXML.item(i).getNodeName().equals("comments"))
-			{
-				NodeList comments = interpretationXML.item(i).getChildNodes();
-				
-				for(int j = 0; j < comments.getLength(); j++)
-				{
-					tempString = comments.item(i).getNodeValue();
-					mInterpretations.setText(tempString);
-				}
-			}
-		}
-		*/
+		//Updates the content
+		showInterpretation(mCurrentInterpretation);
 	}
 	
-	private void showInitialInterpretation(int interpretation)
+	private void showInterpretation(int interNr)
 	{
+		//Removes previous content
+		mCommentsContainer.removeAllViews();
+		mResourceContainer.removeAllViews();
+		//mSpnInterpretationList.setSelection(interNr);
 		
+		//Creates a TextView that shows initial interpretation
+		showInitialComment(interNr);
+		
+		//Load image/chart into the placeholder
+		showChart(interNr);
+		
+		//Creates a TextView for each comment and adds it into the layout
+		showComments(interNr);	
+	}
+	
+	private void showInitialComment(int interNr)
+	{
+		String info = mInterpretationList.get(interNr).mUser.mName + ": ";
+		mUserDateInfo.setText(info);
+		mUserDateInfo.setTextColor(getResources().getColor(R.color.Pink));
+		String text = mInterpretationList.get(interNr).mText;
+		mInitialComment.setText(text);
 	}
 	
 	private void showComments(int interNr)
 	{
-		mLayoutInterpretations.removeAllViews();
-		
 		for(int i = 0; i < mInterpretationList.get(interNr).mCommentThread.size(); i++)
 		{
-			TextView tempView = new TextView(this);
-			tempView.setText(mInterpretationList.get(interNr).mCommentThread.get(i).mText);
-			mLayoutInterpretations.addView(tempView);
-			Log.d("Testing", mInterpretationList.get(interNr).mCommentThread.get(i).mText);
+			TextView tempView1 = new TextView(this);
+			tempView1.setText(mInterpretationList.get(interNr).mCommentThread.get(i).mUser.mName + ": ");
+			tempView1.setTextColor(getResources().getColor(R.color.Pink));
+			mCommentsContainer.addView(tempView1);
+			
+			TextView tempView2 = new TextView(this);
+			tempView2.setText(mInterpretationList.get(interNr).mCommentThread.get(i).mText);
+			mCommentsContainer.addView(tempView2);
+			
+			Log.d("Log Comment Text:", mInterpretationList.get(interNr).mCommentThread.get(i).mText);
 		}
 	}
 	
-	private void showChart(String url, int imageView)
+	private void showChart(int InterNr)
 	{
-		String interpretation = ConnectionManager.getConnectionManager().doRequest(url + ".xml");
-		NodeList nodes = Util.getDomElement(interpretation).getChildNodes().item(0).getChildNodes();
+		//If there is no resource associated with the interpretation
+		if(mInterpretationList.get(InterNr).mInfo.size() < 1)
+		{ 
+			TextView tempView = new TextView(this);
+			tempView.setText("No resource associated with interpretation");
+			mResourceContainer.addView(tempView);
+			//mResourceInformer.setText("No resource associated with interpretation");
+			//if(mResourceSwitcher.getDisplayedChild() == 0)mResourceSwitcher.showNext();
+			return;
+		}
 		
-		for (int i = 0; i < nodes.getLength(); i++) {
-			if (nodes.item(i).getNodeName().equals("chart")) {
-				mImageContainer.setImageDrawable(ConnectionManager.getConnectionManager()
-						.getImage(nodes.item(i).getAttributes().getNamedItem("href").getNodeValue() 
-								+ "/data"));
-				
-				return;
-				
-			}
+		//If there is a link to the associated resource
+		if(!mInterpretationList.get(InterNr).mInfo.get(0).mChart)
+		{
+			TextView tempView = new TextView(this);
+			tempView.setText("TODO: create a link");
+			mResourceContainer.addView(tempView);
+			//mResourceInformer.setText("TODO: create a link");
+			//if(mResourceSwitcher.getDisplayedChild() == 0)mResourceSwitcher.showNext();
+			return;
+		}
+		//If there is a chart to be drawn from the resource
+		else
+		{
+			Drawable drawable = ConnectionManager.getConnectionManager().getImage(mInterpretationList.get(InterNr).mInfo.get(0).getImageUrl());
+			ImageView tempView = new ImageView(this);
+			tempView.setImageDrawable(drawable);
+			mResourceContainer.addView(tempView);
+			//mImageContainer.setImageDrawable(drawable);
+			//if(mResourceSwitcher.getDisplayedChild() == 1)mResourceSwitcher.showPrevious();
 		}
 	}
-
+	
 	void setActivityObjects()
 	{		
 		mSpnInterpretationList = (Spinner) findViewById(R.id.interpretations_spinner);
+		mResourceContainer = (LinearLayout) findViewById(R.id.interpretations_resourceContainer);
+		//mResourceSwitcher = (ViewSwitcher) findViewById(R.id.interpretation_resourceSwitcher);
 		mImageContainer = (ImageView) findViewById(R.id.interpretations_imageContainer);
-		mLayoutInterpretations = (LinearLayout) findViewById(R.id.interpretations_commentContainer);
+		mResourceInformer = (TextView) findViewById(R.id.interpretations_resourceText);
+		mInitialComment = (TextView) findViewById(R.id.interpretations_initCommentText);
+		mUserDateInfo = (TextView) findViewById(R.id.interpretations_initCommentUserDate);
+		mCommentsContainer = (LinearLayout) findViewById(R.id.interpretations_commentContainer);
 		mEditTextInterpretation = (EditText) findViewById(R.id.interpretation_editAddInterpretation);
 		mButtonAddInterpretation = (Button) findViewById(R.id.interpretation_btnAdd);
 		mButtonAddInterpretation.setOnClickListener(new OnClickListener() 
@@ -206,6 +183,8 @@ public class InterpretationActivity extends Activity {
 			public void onClick(View v) 
 			{
 				// TODO Auto-generated method stub
+				mCurrentInterpretation += 1;
+				showInterpretation(mCurrentInterpretation);
 			}
 		});
 		mButtonRefresh = (Button) findViewById(R.id.interpretation_btnRefresh);
